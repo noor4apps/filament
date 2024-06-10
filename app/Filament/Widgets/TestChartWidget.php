3e2fe22406ac6f5060fa4e7ec62eb9eaf6cb30f5
MuhammadNoor <noor4apps@gmail.com>
2024-06-10 21:34:53 +0300

    Charts step2

--- app/Filament/Widgets/TestChartWidget.php
+++ app/Filament/Widgets/TestChartWidget.php
@@ -2,24 +2,35 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Post;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class TestChartWidget extends ChartWidget
 {
     protected static ?string $heading = 'Chart';
 
-    protected int | string | array $columnSpan = 2;
+    protected int|string|array $columnSpan = 2;
 
     protected function getData(): array
     {
+        $data = Trend::model(Post::class)
+            ->between(
+                start: now()->subMonths(6),
+                end: now(),
+            )
+            ->perMonth()
+            ->count();
+
         return [
             'datasets' => [
                 [
                     'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
         ];
     }
 
